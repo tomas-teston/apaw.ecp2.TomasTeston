12b0a7fe1892a568a82134af52508296d512d93e
Tomas Muñoz Testón <tomini18@hotmail.com>
2018-10-09 12:45:46 +0200

    Created "modificar jefe". issue #2

--- src/main/java/api/Dispatcher.java
+++ src/main/java/api/Dispatcher.java
@@ -5,10 +5,13 @@ import api.daos.DaoFactory;
 import api.daos.memory.DaoMemoryFactory;
 import api.dtos.JefeDto;
 import api.exceptions.ArgumentNotValidException;
+import api.exceptions.NotFoundException;
 import api.exceptions.RequestInvalidException;
 import http.HttpRequest;
 import http.HttpResponse;
 import http.HttpStatus;
+
+import static http.HttpMethod.PUT;
 
 public class Dispatcher {
 
@@ -23,9 +26,12 @@ public class Dispatcher {
         String ERROR_MESSAGE = "{'error':'%S'}";
         try {
             switch (request.getMethod()) {
-            case POST:
-                this.doPost(request, response);
-                break;
+                case POST:
+                    this.doPost(request, response);
+                    break;
+                case PUT:
+                    this.doPut(request);
+                    break;
                 default: // Unexpected
                     throw new RequestInvalidException("method error: " + request.getMethod());
             }
@@ -33,6 +39,9 @@ public class Dispatcher {
         } catch(ArgumentNotValidException | RequestInvalidException exception) {
             response.setBody(String.format(ERROR_MESSAGE, exception.getMessage()));
             response.setStatus(HttpStatus.BAD_REQUEST);
+        } catch (NotFoundException exception) {
+            response.setBody(String.format(ERROR_MESSAGE, exception.getMessage()));
+            response.setStatus(HttpStatus.NOT_FOUND);
         } catch(Exception exception) {
             exception.printStackTrace();
             response.setBody(String.format(ERROR_MESSAGE, exception));
@@ -47,5 +56,15 @@ public class Dispatcher {
             throw new RequestInvalidException("request error: " + request.getMethod() + ' ' + request.getPath());
         }
     }
+
+    private void doPut(HttpRequest request) {
+        if (request.isEqualsPath(JefeApiController.JEFES + JefeApiController.ID_ID)) {
+            this.jefeApiController.update(request.getPath(1), (JefeDto) request.getBody());
+        } else {
+            throw new RequestInvalidException("request error: " + request.getMethod() + ' ' + request.getPath());
+        }
+    }
+
+
 
 }
